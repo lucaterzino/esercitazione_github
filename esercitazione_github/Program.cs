36f70aad95d0a6a8b5e68c178592f64908b4af50
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:52:22 +0100

    Add project files.

---
+++ esercitazione_github/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercitazione_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+        }
+    }
+}
