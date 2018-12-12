04a2c3fd919edef209ac868ba5d37d4ef8a8952e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 09:03:18 +0100

    Creazione del codice

--- esercitazione_github/Program.cs
+++ esercitazione_github/Program.cs
@@ -9,7 +9,19 @@ namespace esercitazione_github
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
+            string risultato;
+            int num = 255;
+            risultato = "";
+            while(num >1)
+            {
+                int resto = num % 2;
+                risultato = Convert.ToString(resto) + risultato;
+                num = num / 2;
+            }
+            risultato = Convert.ToString(num) + risultato;
+            Console.WriteLine($"Il numero binario è {risultato}");
+            Console.ReadLine();
         }
     }
 }
